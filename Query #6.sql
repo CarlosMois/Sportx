70a9659b268d0492574a9d2eb7f4fbd2905d1bef
Nacho777-Z <6799217@clases.edu.sv>
2026-07-10 16:37:54 -0600

    Continuación de BD

--- Query #6.sql
+++ Query #6.sql
@@ -1,6 +1,23 @@
 USE Sportx;
 
 CREATE TABLE Usuarios(
-id_usuarios INT AUTO_INCREMENT 
+id_usuarios INT AUTO_INCREMENT;
+nombre VARCHAR(100) NOT NULL;
+correo VARCHAR(250) NOT NULL UNIQUE;
+password_hash VARCHAR(250) NOT NULL;
 
-);
+RolENUM(
+    'usuario'
+    'admin'
+)DEFAULT 'usuario'
+)
+fecha_registro          DATETIME DEFAULT CURRENT_TIMESTAMP 7
+
+estado ENUM(
+    'activo'
+    'suspendido'
+)DEFAULT 'activo'
+
+CREATE TABLE interses_usuarios
+
+
